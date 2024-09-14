4a7daeb618ed2871234fa91fcd25661388552804
Nicole Scribner <nicolescrib@gmail.com>
2024-09-14 13:12:58 -0700

    add command to link scripts from repo

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ echo "Linking .bashrc..."
 ln -s ~/repos/dotfiles/.bashrc ~/.bashrc
 echo "Linking .ssh/config..."
 ln -s ~/repos/dotfiles/.ssh/config ~/.ssh/config
+echo "Linking .screenrc..."
+ln -s ~/repos/dotfiles/.screenrc ~/.screenrc
+echo "Linking scripts..."
+ln -s ~/repos/dotfiles/scripts/* ~/scripts/
