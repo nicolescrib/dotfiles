6b3affedd671142db7294cea905619959ee9512a
Nicole Scribner <nicolescrib@gmail.com>
2024-09-14 13:20:03 -0700

    let setup.sh create ~/scripts if it doesn't exist

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Create links to dotfiles
 echo "Linking .vimrc..."
 ln -s ~/repos/dotfiles/.vimrc ~/.vimrc
 echo "Linking .bashrc..."
@@ -8,5 +9,12 @@ echo "Linking .ssh/config..."
 ln -s ~/repos/dotfiles/.ssh/config ~/.ssh/config
 echo "Linking .screenrc..."
 ln -s ~/repos/dotfiles/.screenrc ~/.screenrc
+
+# Make the scripts directory if it doesn't exist
+if [ ! -d ~/scripts ]; then
+	mkdir ~/scripts
+fi
+
+# Create links to scripts
 echo "Linking scripts..."
 ln -s ~/repos/dotfiles/scripts/* ~/scripts/
